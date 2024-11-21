7a3d355d49195475b3e6ae88a200fcc43d63a0af
ivanartemiev <art.lx.ru@gmail.com>
2024-11-21 10:17:51 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+total_pfmd_task = 12
+total_hour_used = 1.5
+course_name = 'Курс: Python'
+time_used_for_one_task = (total_hour_used/total_pfmd_task)
+print(course_name,', всего задач:',total_pfmd_task,", затрачено времени:",total_hour_used,', среднее время выполнения:',time_used_for_one_task)
